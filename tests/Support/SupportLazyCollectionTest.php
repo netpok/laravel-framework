622dd6fbb1fd5756b3f7d5341fd05914895b5497
netpok <netpok@gmail.com>
2019-10-13 23:34:25 +0200

    Add tests for remeber

--- tests/Support/SupportLazyCollectionTest.php
+++ tests/Support/SupportLazyCollectionTest.php
@@ -80,6 +80,82 @@ class SupportLazyCollectionTest extends TestCase
         $this->assertSame([1, 2, 3, 4, 5], $data->all());
     }
 
+    public function testRemember()
+    {
+        $source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        $calls = 0;
+
+        $data = LazyCollection::make(function () use (&$source, &$calls) {
+            foreach ($source as $item) {
+                $calls++;
+                yield $item;
+            }
+        })->remember();
+
+        $this->assertSame([1, 2, 3], $data->take(3)->all());
+        $this->assertEquals(3, $calls);
+
+        $this->assertSame([1, 3, 5], $data->filter(function ($item) {
+            return $item % 2;
+        })->values()->take(3)->all());
+        $this->assertEquals(5, $calls);
+
+        $this->assertSame([
+            0 => 1,
+            2 => 3,
+            4 => 5
+        ], $data->filter(function ($item) {
+            return $item % 2;
+        })->take(3)->all());
+        $this->assertEquals(5, $calls);
+
+        $this->assertSame($source, $data->all());
+        $this->assertEquals(count($source), $calls);
+    }
+
+    public function testRememberWithKeys()
+    {
+        $source = [
+            'a' => 1,
+            'b' => 2,
+            'c' => 3,
+            'd' => 4,
+        ];
+        $calls = 0;
+
+        $data = LazyCollection::make(function () use ($source, &$calls) {
+            foreach ($source as $key => $item) {
+                $calls++;
+                yield $key=>$item;
+            }
+        })->remember();
+
+        $this->assertSame([
+            'a' => 1,
+            'b' => 2,
+        ], $data->take(2)->all());
+        $this->assertEquals(2, $calls);
+
+        $this->assertSame([
+            'a' => 1,
+            'c' => 3,
+        ], $data->filter(function ($item, $key) {
+            return $key != 'b';
+        })->take(2)->all());
+        $this->assertEquals(3, $calls);
+
+        $this->assertSame([
+            'a' => 1,
+            'c' => 3,
+        ], $data->filter(function ($item) {
+            return $item % 2;
+        })->all());
+        $this->assertEquals(4, $calls);
+
+        $this->assertSame($source, $data->all());
+        $this->assertEquals(count($source), $calls);
+    }
+
     public function testTapEach()
     {
         $data = LazyCollection::times(10);
